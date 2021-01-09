916be9dc43de057c1b45b51010bb4f62083c06fc
Tom van Nes <development.nes@gmail.com>
2021-01-09 17:05:07 +0100

    new photo

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Header() {
     return (
         <header className={styles.container}>
-            <div className={styles["header-section"]}>
+            <nav className={styles["header-section"]}>
                 <Link href="/">
                     <a>Home</a>
                 </Link>
@@ -14,21 +14,13 @@ export default function Header() {
                 <Link href="/projects">
                     <a>Projects</a>
                 </Link>
-            </div>
+            </nav>
             <div className={styles["header-section"]}>
                 <a href="https://www.linkedin.com/in/tom-van-nes-5523a512a/">
-                    <img
-                        className={styles["img-logo"]}
-                        src="/pngkit_linkedin-icon-png_85849.png"
-                        alt="Linkedin logo"
-                    />
+                    <img className={styles["img-logo"]} src="/pngkit_linkedin-icon-png_85849.png" alt="Linkedin logo" />
                 </a>
                 <a href="https://github.com/tomnes94">
-                    <img
-                        className={styles["img-logo"]}
-                        src="/GitHub-Mark-32px.png"
-                        alt="Github logo"
-                    />
+                    <img className={styles["img-logo"]} src="/GitHub-Mark-32px.png" alt="Github logo" />
                 </a>
             </div>
         </header>
