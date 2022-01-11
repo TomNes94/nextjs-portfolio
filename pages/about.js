775714aaa3f7ddc93d1af3d18f4a830f9ae28e5f
Tom van Nes <development.nes@gmail.com>
2022-01-11 11:35:24 +0100

    update portfolio

--- pages/about.js
+++ pages/about.js
@@ -2,90 +2,111 @@ import Header from "../components/Header";
 import Card from "../components/Card";
 import Head from "next/head";
 import { Chrono } from "react-chrono";
-import Image from "next/image";
 import styles from "../styles/About.module.scss";
 
 export default function About() {
-    const items = [
-        {
-            title: "Sep 2012 - Apr 2016",
-            cardTitle: "Bsc Economics and Business Economics",
-            cardSubtitle: "Rijksuniversiteit Groningen",
-        },
-        {
-            title: "Sep 2016 - Feb 2018",
-            cardTitle: "MSc Economics",
-            cardSubtitle: "Rijksuniversiteit Groningen",
-        },
-        {
-            title: "Sep 2017 - Apr 2020",
-            cardTitle: "MSc Real Estate Studies",
-            cardSubtitle: "Rijksuniversiteit Groningen",
-        },
-        {
-            title: "Sep 2018 - Current",
-            cardTitle: "Web Developer",
-            cardSubtitle: "ValueMetrics B.V.",
-        },
-    ];
+  const items = [
+    {
+      title: "Sep 2012 - Apr 2016",
+      cardTitle: "Bsc Economics and Business Economics",
+      cardSubtitle: "Rijksuniversiteit Groningen",
+    },
+    {
+      title: "Sep 2016 - Feb 2018",
+      cardTitle: "MSc Economics",
+      cardSubtitle: "Rijksuniversiteit Groningen",
+    },
+    {
+      title: "Sep 2017 - Apr 2020",
+      cardTitle: "MSc Real Estate Studies",
+      cardSubtitle: "Rijksuniversiteit Groningen",
+    },
+    {
+      title: "Sep 2018 - Jun 2021",
+      cardTitle: "Web Developer",
+      cardSubtitle: "ValueMetrics B.V.",
+    },
+    {
+      title: "Jul 2021 - Current",
+      cardTitle: "Fullstack Javascript Developer",
+      cardSubtitle: "LINKIT B.V.",
+    },
+  ];
 
-    return (
-        <div className={styles.container}>
-            <Head>
-                <title>Tom van Nes - Web developer</title>
-                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-                <meta name="description" content="This is the about page of Tom van Nes - junior web developer." />
-                <meta name="robots" content="index, follow" />
-                <meta charSet="UTF-8" />
-            </Head>
-            <Header />
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Tom van Nes - Web developer</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="description"
+          content="This is the about page of Tom van Nes - web developer."
+        />
+        <meta name="robots" content="index, follow" />
+        <meta charSet="UTF-8" />
+      </Head>
+      <Header />
 
-            <main className={styles.main}>
-                <h1>About me</h1>
-                <div className={styles.subsection}>
-                    <div className={styles["card-container"]}>
-                        <Card size="large">
-                            <p>
-                                I am Tom van Nes, living in Utrecht for a few years now. What started as hobby is now an amazing day job: since 2 years I do
-                                front- and backend programming professionally. I make a UI do what it functionally should do. Preferably NodeJS, Vue.js, and
-                                Typescript. I try do so by learning something new everyday.
-                            </p>
-                            <p>Off-screen I like to play soccer, over-the-top cooking and am proud indoor gardener.</p>
-                        </Card>
-                        <Card title="Skills" size="large">
-                            <div className={styles["horizontal-card"]}>
-                                <ul>
-                                    <li>NodeJS</li>
-                                    <li>Typescript/ES6</li>
-                                    <li>SQL Server/PostgreSQL</li>
-                                </ul>
-                                <ul>
-                                    <li>VueJS</li>
-                                    <li>HTML5/(S)CSS</li>
-                                </ul>
-                            </div>
-                        </Card>
-                    </div>
-                    <div className={styles["card-container"]}>
-                        <img className={styles["personal-image"]} src="/personal_photo.jpg" alt="Photo of me" />
-                    </div>
-                </div>
-                <div className={styles["card-container"]}>
-                    <Card size="medium" margin="my">
-                        <Chrono
-                            style={{ fontSize: "10px!important" }}
-                            items={items}
-                            mode="VERTICAL"
-                            hideControls={true}
-                            theme={{ primary: "black", secondary: "transparent", cardBgColor: "white", cardForeColor: "black" }}
-                        />
-                    </Card>
-                </div>
-            </main>
-
-            <footer className={styles.footer}>
-                <p>{`Tom van Nes © ${new Date().getFullYear()}`}</p>
-            </footer>
+      <main className={styles.main}>
+        <h1>About me</h1>
+        <div className={styles.subsection}>
+          <div className={styles["card-container"]}>
+            <Card size="large">
+              <p>
+                I am Tom van Nes, living in Utrecht for a few years now. What
+                started as hobby is now an amazing day job: since 2 years I do
+                front- and backend programming professionally. I make a UI do
+                what it functionally should do. Preferably NodeJS, Vue.js, and
+                Typescript. I try do so by learning something new everyday.
+              </p>
+              <p>
+                Off-screen I like to play soccer, over-the-top cooking and am
+                proud indoor gardener.
+              </p>
+            </Card>
+            <Card title="Skills" size="large">
+              <div className={styles["horizontal-card"]}>
+                <ul>
+                  <li>NodeJS</li>
+                  <li>Typescript/ES6</li>
+                  <li>SQL Server/PostgreSQL</li>
+                </ul>
+                <ul>
+                  <li>VueJS</li>
+                  <li>HTML5/(S)CSS</li>
+                </ul>
+              </div>
+            </Card>
+          </div>
+          <div className={styles["card-container"]}>
+            <img
+              className={styles["personal-image"]}
+              src="/personal_photo.jpg"
+              alt="Photo of me"
+            />
+          </div>
         </div>
-    );
+        <div className={styles["card-container"]}>
+          <Card size="medium" margin="my">
+            <Chrono
+              style={{ fontSize: "10px!important" }}
+              items={items}
+              mode="VERTICAL"
+              hideControls={true}
+              theme={{
+                primary: "black",
+                secondary: "transparent",
+                cardBgColor: "white",
+                cardForeColor: "black",
+              }}
+            />
+          </Card>
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <p>{`Tom van Nes © ${new Date().getFullYear()}`}</p>
+      </footer>
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -5,29 +5,34 @@ import Head from "next/head";
 import styles from "../styles/Home.module.scss";
 
 export default function Home() {
-    return (
-        <div className={styles.container}>
-            <Head>
-                <title>Tom van Nes - Web developer</title>
-                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-                <meta name="description" content="This is the portfolio home page of Tom van Nes - junior web developer." />
-                <meta name="robots" content="index, follow" />
-                <meta charSet="UTF-8" />
-            </Head>
-            <Header />
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Tom van Nes - Web developer</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="description"
+          content="This is the portfolio home page of Tom van Nes - web developer."
+        />
+        <meta name="robots" content="index, follow" />
+        <meta charSet="UTF-8" />
+      </Head>
+      <Header />
 
-            <main className={styles.main}>
-                <Card title="Hi!" photoSrc="/photo.jpg" size="small">
-                    <p>
-                        My name is Tom van Nes. I am a web developer working at ValueMetrics. Creating web application backends and supporting user interfaces
-                        is what I do.
-                    </p>
-                </Card>
-            </main>
+      <main className={styles.main}>
+        <Card title="Hi!" photoSrc="/photo.jpg" size="small">
+          <p>
+            My name is Tom van Nes. I am a web developer working at LINKIT.
+            Creating web application backends and supporting user interfaces is
+            what I do.
+          </p>
+        </Card>
+      </main>
 
-            <footer className={styles.footer}>
-                <p>Tom van Nes © {new Date().getFullYear()}</p>
-            </footer>
-        </div>
-    );
+      <footer className={styles.footer}>
+        <p>Tom van Nes © {new Date().getFullYear()}</p>
+        <a href="mailto: development.nes@gmail.com">Send Email</a>
+      </footer>
+    </div>
+  );
 }
